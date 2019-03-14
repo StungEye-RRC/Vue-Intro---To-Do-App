2f72f0f174c4bab8d60aa414ae02106c4883a776
Kyle Geske <stungeye@gmail.com>
2019-03-14 17:51:25 -0500

    Added the serialized storage of todo items in local storage.

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
+let serializedStorage = {
+  STORAGE_KEY: "our_vuejs_to_dos",
+  fetch: function() {
+    const serialized_to_dos =
+      localStorage.getItem(serializedStorage.STORAGE_KEY) || "[]";
+    return JSON.parse(serialized_to_dos);
+  },
+
+  save: function(to_dos) {
+    const serialized_to_dos = JSON.stringify(to_dos);
+    localStorage.setItem(serializedStorage.STORAGE_KEY, serialized_to_dos);
+  }
+};
+
 new Vue({
   el: "#app",
 
   data: {
     new_todo_item: "",
-    to_dos: [
-      { text: "Finish Vue Js Lecture", done: false },
-      { text: "Watch Vue Js Intro Video", done: true },
-      { text: "Learn Javascript Programming", done: true }
-    ]
+    to_dos: serializedStorage.fetch()
   },
 
   methods: {
@@ -20,6 +30,17 @@ new Vue({
 
       // Clear out the input that is bound to the new_todo_item.
       this.new_todo_item = "";
+    },
+
+    clear_completed: function() {
+      this.to_dos = this.to_dos.filter(todo => !todo.done);
+    }
+  },
+
+  watch: {
+    to_dos: {
+      handler: serializedStorage.save,
+      deep: true
     }
   }
 });
