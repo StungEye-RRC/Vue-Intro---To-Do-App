823151451e097666e98558612d58eceefec7a554
Kyle Geske <stungeye@gmail.com>
2019-03-14 17:17:49 -0500

    Implemented the add item functionality.

--- index.js
+++ index.js
@@ -10,5 +10,16 @@ new Vue({
     ]
   },
 
-  methods: {}
+  methods: {
+    add_item: function() {
+      // Guard statement to prevent empty todo items from being added.
+      if (this.new_todo_item === "") return;
+
+      // Add the new item to the to_dos array.
+      this.to_dos.push({ text: this.new_todo_item, done: false });
+
+      // Clear out the input that is bound to the new_todo_item.
+      this.new_todo_item = "";
+    }
+  }
 });
