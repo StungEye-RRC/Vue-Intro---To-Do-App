534a4599c768bbe2795d3014f9d392f35c8ad557
Kyle Geske <stungeye@gmail.com>
2019-03-14 17:06:23 -0500

    Displaying the todo list and added the ability to toggle the done state.

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ new Vue({
   el: "#app",
 
   data: {
-    new_todo_item: "Hello World"
+    new_todo_item: "",
+    to_dos: [
+      { text: "Finish Vue Js Lecture", done: false },
+      { text: "Watch Vue Js Intro Video", done: true },
+      { text: "Learn Javascript Programming", done: true }
+    ]
   },
 
   methods: {}
